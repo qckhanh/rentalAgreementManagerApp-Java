3f5e5b5073370a5b0a2cf4fd972c8abced93b399
Quoc Khanh Khong <khanhquoc1125@gmail.com>
2024-12-04 20:19:28 +0700

    delete

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example._4knightsasm2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example._4knightsasm2 to javafx.fxml;
-    exports org.example._4knightsasm2;
-}
