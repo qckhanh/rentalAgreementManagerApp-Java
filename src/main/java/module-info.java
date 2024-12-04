dd13aa1aa7444621a0a180a8b077dc351c750c9d
Quoc Khanh Khong <khanhquoc1125@gmail.com>
2024-12-04 23:26:05 +0700

    innit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.example._4knightsasm2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.example._4knightsasm2 to javafx.fxml;
+    exports org.example._4knightsasm2;
+}
