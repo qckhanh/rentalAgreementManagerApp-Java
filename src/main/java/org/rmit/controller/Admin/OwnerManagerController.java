5e3b16e86c14a6bf8851112fab939ac9a44238b9
Đào Đức Lam <s4019052@rmit.edu.vn>
2024-12-31 23:17:45 +0700

    full table view

--- src/main/java/org/rmit/controller/Admin/OwnerManagerController.java
+++ src/main/java/org/rmit/controller/Admin/OwnerManagerController.java
@@ -1,26 +1,51 @@
 package org.rmit.controller.Admin;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.rmit.database.HostDAO;
+import org.rmit.database.OwnerDAO;
+import org.rmit.model.Persons.Host;
+import org.rmit.model.Persons.Owner;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class OwnerManagerController implements Initializable {
     public Label welcomeLabel;
-    public TableView properties_tableView;
     public ComboBox propertyTypeFilter_comboBox;
     public ComboBox propertyStatusFilter_comboBox;
     public Button addOwnerButton;
     public Button updateOwnerButton;
     public Button deleteOwnerButton;
     public Button readOwnerButton;
+    public TableView owners_tableView;
+    private ObservableList<Owner> ownerObservableList = FXCollections.observableArrayList();
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        owners_tableView.getColumns().addAll(
+                createColumn("Owner ID", "id"),
+                createColumn("Owner Contact", "contact"),
+                createColumn("Owner Name", "name")
+        );
+        owners_tableView.setItems(ownerObservableList);
+        OwnerDAO ownerDAO = new OwnerDAO();
+        List<Owner> list = ownerDAO.getAll();
+        loadData(list);
+    }
 
+    private TableColumn<Owner, ?> createColumn(String columnName, String propertyName) {
+        TableColumn<Owner, ?> column = new TableColumn<>(columnName);
+        column.setCellValueFactory(new PropertyValueFactory<>(propertyName));
+        return column;
+    }
+
+    private void loadData(List<Owner> list) {
+        ownerObservableList.setAll(list);
     }
 }
--- src/main/java/org/rmit/controller/Admin/PaymentManagerController.java
+++ src/main/java/org/rmit/controller/Admin/PaymentManagerController.java
@@ -1,26 +1,50 @@
 package org.rmit.controller.Admin;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.rmit.database.HostDAO;
+import org.rmit.database.PaymentDAO;
+import org.rmit.model.Agreement.Payment;
+import org.rmit.model.Persons.Host;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class PaymentManagerController implements Initializable {
     public Label welcomeLabel;
-    public TableView properties_tableView;
     public ComboBox propertyTypeFilter_comboBox;
     public ComboBox propertyStatusFilter_comboBox;
     public Button addPaymentButton;
     public Button updatePaymentButton;
     public Button deletePaymentButton;
     public Button readPaymentButton;
+    public TableView payments_tableView;
+    private ObservableList<Payment> paymentObservableList = FXCollections.observableArrayList();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        payments_tableView.getColumns().addAll(
+                createColumn("Payment ID", "paymentId"),
+                createColumn("Date", "date"),
+                createColumn("Rental Agreement", "rentalAgreement")
+        );
+        payments_tableView.setItems(paymentObservableList);
+        PaymentDAO paymentDAO = new PaymentDAO();
+        List<Payment> list = paymentDAO.getAll();
+        loadData(list);
+    }
 
+    private TableColumn<Payment, ?> createColumn(String columnName, String propertyName) {
+        TableColumn<Payment, ?> column = new TableColumn<>(columnName);
+        column.setCellValueFactory(new PropertyValueFactory<>(propertyName));
+        return column;
+    }
+
+    private void loadData(List<Payment> list) {
+        paymentObservableList.setAll(list);
     }
 }
