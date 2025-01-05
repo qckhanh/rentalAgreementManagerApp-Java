1b77151695c9dbda585c7dac85dd40a8d6e4372e
lbibi1805-dot <chibachluong@gmail.com>
2025-01-05 10:28:37 +0700

    Input_Validator

--- src/main/java/org/rmit/controller/Host/Host_EditProfileController.java
+++ src/main/java/org/rmit/controller/Host/Host_EditProfileController.java
@@ -1,14 +1,13 @@
 package org.rmit.controller.Host;
 
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
+import net.synedra.validatorfx.Validator;
 import org.kordamp.ikonli.feather.Feather;
 import org.kordamp.ikonli.javafx.FontIcon;
 import org.rmit.Helper.ImageUtils;
+import org.rmit.Helper.InputValidator;
 import org.rmit.Helper.UIDecorator;
 import org.rmit.database.DAOInterface;
 import org.rmit.database.HostDAO;
@@ -20,6 +19,7 @@ import org.rmit.model.Persons.Renter;
 import org.rmit.model.Session;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
@@ -35,6 +35,13 @@ public class Host_EditProfileController implements Initializable {
     public ImageView avatar_ImageView;
     public Button avatarUpdate_btn;
     public String SELECTED_PATH = ImageUtils.DEFAULT_IMAGE;
+    public Label name_err;
+    public Label contact_err;
+    public Label dob_err;
+    public Label username_err;
+    public Label password_err;
+
+    Validator validator = new Validator();
 
     Person currentUser = Session.getInstance().getCurrentUser();
 
@@ -47,6 +54,8 @@ public class Host_EditProfileController implements Initializable {
         edit_btn.setDisable(false);
         edit_btn.setOnAction(event -> editProfile());
         avatarUpdate_btn.setOnAction(event -> updateAvatar());
+        resetErrorLabels();
+        validateInput();
     }
 
     private void innitText(){
@@ -60,11 +69,26 @@ public class Host_EditProfileController implements Initializable {
     }
 
     private void addListener(){
-        newName_input.textProperty().addListener((observable, oldValue, newValue) -> checkForChanges());
-        newContact_input.textProperty().addListener((observable, oldValue, newValue) -> checkForChanges());
-        newDOB_input.valueProperty().addListener((observable, oldValue, newValue) -> checkForChanges());
-        newPassword_input.textProperty().addListener((observable, oldValue, newValue) -> checkForChanges());
-        newUsername_input.textProperty().addListener((observable, oldValue, newValue) -> checkForChanges());
+        newName_input.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.equals(oldValue)) name_err.setText("");
+            checkForChanges();
+        });
+        newContact_input.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.equals(oldValue)) contact_err.setText("");
+            checkForChanges();
+        });
+        newDOB_input.valueProperty().addListener((observable, oldValue, newValue) -> {
+            if (!Objects.equals(oldValue, newValue)) dob_err.setText("");
+            checkForChanges();
+        });
+        newPassword_input.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.equals(oldValue)) password_err.setText("");
+            checkForChanges();
+        });
+        newUsername_input.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.equals(oldValue)) username_err.setText("");
+            checkForChanges();
+        });
         currentUser.profileAvatarPropertyProperty().addListener((observable, oldValue, newValue) -> {
             avatar_ImageView.setImage(ImageUtils.byteToImage(newValue));
         });
@@ -75,12 +99,73 @@ public class Host_EditProfileController implements Initializable {
         UIDecorator.buttonIcon(avatarUpdate_btn, EDIT);
     }
 
+    private void validateInput() {
+        validator.createCheck()
+                .dependsOn("newName", newName_input.textProperty())
+                .withMethod(context -> {
+                    String input = context.get("newName");
+                    if (!InputValidator.NoCondition(input, name_err)) {
+                        context.error("Name must not be empty");
+                    }
+                })
+                .decorates(newName_input)
+                .immediateClear();
+
+        validator.createCheck()
+                .dependsOn("newUsername", newUsername_input.textProperty())
+                .withMethod(context -> {
+                    String input = context.get("newUsername");
+                    if (!InputValidator.isValidUsername(input, username_err)) {
+                        context.error("Username must be at least 6 characters");
+                    }
+                })
+                .decorates(newUsername_input)
+                .immediateClear();
+
+        validator.createCheck()
+                .dependsOn("newContact", newContact_input.textProperty())
+                .withMethod(context -> {
+                    String input = context.get("newContact");
+                    if (!InputValidator.isValidContact(input, contact_err)) {
+                        context.error("Invalid Phone Number");
+                    }
+                })
+                .decorates(newContact_input)
+                .immediateClear();
+
+        validator.createCheck()
+                .dependsOn("newDOB", newDOB_input.valueProperty())
+                .withMethod(context -> {
+                    LocalDate input = context.get("newDOB");
+                    if (!InputValidator.isValidDateFormat(input, dob_err)) {
+                        context.error("Invalid Date");
+                    }
+                })
+                .decorates(newDOB_input)
+                .immediateClear();
+
+        validator.createCheck()
+                .dependsOn("newPassword", newPassword_input.textProperty())
+                .withMethod(context -> {
+                    String input = context.get("newPassword");
+                    if (!InputValidator.isValidPassword(input, password_err)) {
+                        context.error("Password must be at least 8 characters");
+                    }
+                })
+                .decorates(newPassword_input)
+                .immediateClear();
+
+        edit_btn.disableProperty().bind(validator.containsErrorsProperty());
+    }
+
     private void editProfile(){
         if (edit_btn.getText().equals("Save")) {
-            // Invoke confirm message
-            boolean confirmed = ModelCentral.getInstance().getStartViewFactory().confirmMessage("Are you sure you want to save changes?");
-            if (confirmed) {
-                saveChanges();
+            if (validator.validate()) {
+                // Invoke confirm message
+                boolean confirmed = ModelCentral.getInstance().getStartViewFactory().confirmMessage("Are you sure you want to save changes?");
+                if (confirmed) {
+                    saveChanges();
+                }
             }
         } else {
             setDisableAll(false);
@@ -138,5 +223,13 @@ public class Host_EditProfileController implements Initializable {
         newUsername_input.setDisable(status);
         avatarUpdate_btn.setDisable(status);
         edit_btn.setDisable(status);
+    }
+
+    private void resetErrorLabels(){
+        name_err.setText("");
+        username_err.setText("");
+        contact_err.setText("");
+        dob_err.setText("");
+        password_err.setText("");
     }
 }
