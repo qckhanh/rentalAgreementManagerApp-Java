96e1ce4e95e9137dc7984447e7ffc3a92c98af62
Đào Đức Lam <s4019052@rmit.edu.vn>
2025-01-07 04:15:53 +0700

    hot fix for host, new dashboard, refresh data button

--- src/main/java/org/rmit/controller/Owner/Owner_DashboardController.java
+++ src/main/java/org/rmit/controller/Owner/Owner_DashboardController.java
@@ -6,7 +6,9 @@ import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import org.rmit.Helper.EntityGraphUtils;
 import org.rmit.Notification.Notification;
+import org.rmit.database.OwnerDAO;
 import org.rmit.model.ModelCentral;
 import org.rmit.model.Persons.Owner;
 import org.rmit.model.Persons.Person;
@@ -15,12 +17,18 @@ import org.rmit.model.Session;
 import org.rmit.view.Owner.OWNER_MENU_OPTION;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.*;
 
 public class Owner_DashboardController implements Initializable {
     public Label welcomeLabel;
     public TableView<Notification> recentNotiTableView;
     private final ObservableList<Notification> notificationObservableList = FXCollections.observableArrayList();
+    public Label dateShow;
+    public Label propertiesValueLabel;
+    public Label hostsValueLabel;
+    public Button refreshButton;
+    public ObjectProperty<Person> currentUser = Session.getInstance().currentUserProperty();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -35,7 +43,24 @@ public class Owner_DashboardController implements Initializable {
                 createColumn("Time", "timestamp")
         );
         recentNotiTableView.setItems(notificationObservableList);
+        String text = LocalDate.now().getDayOfWeek().toString() + ", " +
+                LocalDate.now().getDayOfMonth() + " " +
+                LocalDate.now().getMonth().toString() + " " +
+                LocalDate.now().getYear();
+        dateShow.setText(text);
 
+        propertiesValueLabel.setText(String.valueOf(((Owner) Session.getInstance().getCurrentUser()).getPropertiesOwned().size()));
+        hostsValueLabel.setText(String.valueOf(((Owner) Session.getInstance().getCurrentUser()).getHosts().size()));
+        refreshButton.setOnAction(e -> refreshData());
+
+    }
+
+    private void refreshData() {
+        OwnerDAO ownerDAO = new OwnerDAO();
+        int id = Integer.parseInt(currentUser.get().getId()+"");
+        Owner owner = ownerDAO.get(id, EntityGraphUtils::SimpleOwnerFull);
+        propertiesValueLabel.setText(String.valueOf(owner.getPropertiesOwned().size()));
+        hostsValueLabel.setText(String.valueOf(owner.getHosts().size()));
     }
 
     private void loadRecentNotification(Set<Notification> s) {
